ec484202714c326dbcda727cc2e60ab2f6b58e12
James Kim <jhlink@users.noreply.github.com>
2018-03-06 22:18:35 -0500

    feat: Implement GetCategory wrapper in utils/serverAPI.js

--- frontend/src/utils/serverAPI.js
+++ frontend/src/utils/serverAPI.js
@@ -1,1 +1,12 @@
-const AUTH_KEY = "dev_test_1"
+const AUTH_KEY = 'dev_test_1';
+const API_HOST = 'http://localhost:3001';
+
+const reqHeaders = {
+	'Accept': 'application/json',
+	'Authorization': AUTH_KEY 
+};
+
+export const GetCategories = () => 
+	fetch(`${API_HOST}/categories`, { 
+		headers: reqHeaders
+	}).then((res) => res.json());
