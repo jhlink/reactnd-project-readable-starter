303d49df0ee1055ad4efea86556ad3270d450033
James Kim <jhlink@users.noreply.github.com>
2018-03-06 22:33:43 -0500

    feat: Implement basic skeleton for Category component with PropTypes

---
+++ frontend/src/components/Category.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types'; 
+
+const Category = (props) => {
+	const { name, path } = props; 
+
+	return (
+		<li key={path}>{name}</li> 
+	);
+};
+
+Category.propTypes = {
+	name: PropTypes.string.isRequired,
+	path: PropTypes.string.isRequired
+};
