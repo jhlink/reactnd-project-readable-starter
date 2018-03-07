cae7da80ba06e5a0585b403a2993d81d26d78fd5
James Kim <jhlink@users.noreply.github.com>
2018-03-07 02:20:57 -0500

    feat: Add GetCategory to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as ServerAPI from './utils/serverAPI.js';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      backend: 'backend-data'
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			categories: []
+		};
+	}
 
-  componentDidMount() {
-    const url = `${process.env.REACT_APP_BACKEND}/categories`;
-    console.log('fetching from url', url);
-    fetch(url, { headers: { 'Authorization': 'whatever-you-want' },
-                 credentials: 'include' } )
-      .then( (res) => { return(res.text()) })
-      .then((data) => {
-        this.setState({backend:data});
-      });
-  }
+	componentDidMount() {
+		ServerAPI.GetCategories()
+			.then((data) => {
+				console.log(data);
+				this.setState(data);
+			});
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Talking to the backend yields these categories: <br/>
-          {this.state.backend}
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<div className="App-header">
+					<ul className=".header">
+						{this.state.categories.map(category => (
+							<li key={category.path}>
+								{category.name} with path of: {category.path}
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
