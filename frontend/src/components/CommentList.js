60e16892aab9db945ef06f2644b11a041ce5d286
James Kim <jhlink@users.noreply.github.com>
2018-03-13 05:43:58 -0400

    feat: Create boilerplate CommentList component class

---
+++ frontend/src/components/CommentList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Comment from './Post';
+
+const CommentList = (props) => {
+	const { comments } = props;
+
+	return (
+		<ul className="column content">
+			{comments.map((comment) => (
+				<li key={comment.id} className="comment">
+					<Comment comment={ comment }/>
+				</li>
+			))}
+		</ul>
+	);
+};
+
+CommentList.propTypes = {
+	comments: PropTypes.array.isRequired
+};
+
+
+export default CommentList;
