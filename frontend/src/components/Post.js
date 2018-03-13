17bdd124509a9032458bcbc6d9b0711b43ccb4dd
James Kim <jhlink@users.noreply.github.com>
2018-03-13 02:55:22 -0400

    fix: Update Post component with corrected handling of incoming props

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Post extends Component {
-	componentWillReceiveProps(nextProps) {
-		console.log(nextProps);  
-	}
 
 	render() {
+		console.log(this.props.post);
   
-		const { id, timestamp, title, body, author, category, voteScore, deleted } = this.props;
+		const { id, timestamp, title, body, author, category, voteScore, deleted } = this.props.post;
 
 		return (
-			<div className=".post">
+			<div>
 				<h2> { category } </h2>
 				<h3>{ title } by {author }</h3>
 				<p> { body } </p>  
@@ -22,14 +20,7 @@ class Post extends Component {
 };
 
 Post.propTypes = {
-	id: PropTypes.string.isRequired,
-	timestamp: PropTypes.number.isRequired,
-	title: PropTypes.string.isRequired,
-	body: PropTypes.string.isRequired,
-	author: PropTypes.string.isRequired,
-	category: PropTypes.string.isRequired,
-	voteScore: PropTypes.number.isRequired,
-	deleted: PropTypes.bool.isRequired
+	post: PropTypes.object.isRequired
 };
 
 
