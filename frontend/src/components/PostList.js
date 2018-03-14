f3a2587af2dbe5b5ca3d19c2b7756743f18d83b2
James Kim <jhlink@users.noreply.github.com>
2018-03-14 04:11:27 -0400

    feat: Convert PostList ES6 component into class based Stateless Functional component (SFC)

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Post from './Post';
 
-const PostList = (props) => {
-	const { posts } = props;
+class PostList extends Component {
+  
+	render() {
+		const { posts } = this.props;
 
-	return (
-		<ul className="column content">
-			{posts.map((post) => (
-				<li key={post.id} className="post">
-					<Post post={ post }/>
-				</li>
-			))}
-		</ul>
-	);
+		return (
+			<ul className="column content">
+				{posts.map((post) => (
+					<li key={post.id} className="post">
+						<Post post={ post }/>
+					</li>
+				))}
+			</ul>
+		);
+	}
 };
 
 PostList.propTypes = {
