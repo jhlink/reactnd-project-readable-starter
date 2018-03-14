c2e7e75de5c990e90490b464d8019bd6e6b5016c
James Kim <jhlink@users.noreply.github.com>
2018-03-14 03:54:35 -0400

    feat: Implement handleSelectedCategory with dispatchw

--- frontend/src/components/NavHeader.js
+++ frontend/src/components/NavHeader.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Category from './Category.js';
 import { connect } from 'react-redux';
+import { loadCategoryPosts } from '../actions';
 
 class NavHeader extends Component {
+
+  handleSelectedCategory = (categoryName) => {
+    console.log(categoryName);
+    this.props.dispatch(loadCategoryPosts(categoryName))
+  }
 
 	render() {
 		const { categories } = this.props;
@@ -13,7 +19,7 @@ class NavHeader extends Component {
 				<h1 className=".header">Categories</h1>
 				<ul className=".nav">
 					{categories.map( c => 
-						<li key = { c.path }>
+						<li key = { c.path } onClick={(e) => this.handleSelectedCategory(e.target.textContent)}>
 							<Category category= { c } />
 						</li>
 					)}
