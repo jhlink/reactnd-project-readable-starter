6724ecff4551531b0e0e97d60e8772c4a5961192
James Kim <jhlink@users.noreply.github.com>
2018-03-14 03:45:29 -0400

    feat: Change NavHeader component from ES6 stateless component to class based functional component

--- frontend/src/components/NavHeader.js
+++ frontend/src/components/NavHeader.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Category from './Category.js';
 
-const NavHeader = (props) => {
-	const { categories } = props;
+class NavHeader extends Component {
 
-	return (
-		<div>
-			<h1 className=".header">Categories</h1>
-			<ul className=".nav">
-				{categories.map( c => 
-					<li key = { c.path }>
-						<Category category= { c } />
-					</li>
-				)}
-			</ul>
-		</div>
-	);
+	render() {
+		const { categories } = this.props;
+
+		return (
+			<div>
+				<h1 className=".header">Categories</h1>
+				<ul className=".nav">
+					{categories.map( c => 
+						<li key = { c.path }>
+							<Category category= { c } />
+						</li>
+					)}
+				</ul>
+			</div>
+		);
+	}
 };
 
 NavHeader.propTypes = {
