dabb2f21091b3fca3ed81201b623166f12ed3e5b
James Kim <jhlink@users.noreply.github.com>
2018-03-14 04:09:39 -0400

    fix: Add initial state to categoryPostLoader in reducers

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,15 +2,15 @@ import {
 	LOAD_CATEGORY_POSTS
 } from '../actions';
 
-function categoryPostLoader (state, action) {
+function categoryPostLoader (state = {}, action) {
 	const { categoryId } = action;
 
 	if ( action.type === LOAD_CATEGORY_POSTS ) {
-		console.log(state);
+		console.log('Reducer Output: ', state);
 		return {
 			...state,
 		};
-	}   
+	}
 }
 
 export default categoryPostLoader;
