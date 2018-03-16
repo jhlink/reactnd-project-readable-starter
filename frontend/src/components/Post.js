aaf2f1897ab62c1ccbd104c95eeaf86fe4a388ad
James Kim <jhlink@users.noreply.github.com>
2018-03-15 23:11:43 -0400

    refactor: Update Post to presentational ES6 component

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-class Post extends Component {
+const Post = (props) => {
+	const { id, timestamp, title, body, author, category, voteScore, deleted } = props.post;
 
-	render() {
-		console.log(this.props.post);
-  
-		const { id, timestamp, title, body, author, category, voteScore, deleted } = this.props.post;
-
-		return (
-			<div>
-				<h2> { category } </h2>
-				<h3>{ title } by {author }</h3>
-				<p> { body } </p>  
-				<div> { voteScore } </div>
-			</div>
-		);
-	}
+	return (
+		<div>
+			<h2> { category } </h2>
+			<h3>{ title } by {author }</h3>
+			<p> { body } </p>  
+			<div> { voteScore } </div>
+		</div>
+	);
 };
 
 Post.propTypes = {
 	post: PropTypes.object.isRequired
 };
-
 
 export default Post;
