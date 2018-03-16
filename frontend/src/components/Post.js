44835e2d2be8deffe7460ba3d4562de46bd8c210
James Kim <jhlink@users.noreply.github.com>
2018-03-15 23:13:05 -0400

    fix: Addresses unused values warning in Post for id, timestamp, deleted

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 const Post = (props) => {
-	const { id, timestamp, title, body, author, category, voteScore, deleted } = props.post;
+	const {  timestamp, title, body, author, category, voteScore, deleted } = props.post;
+	const hideFromDelete = deleted ? 'none' : 'block';
+	const formattedDated = new Date(timestamp).toLocaleString();
 
 	return (
-		<div>
+		<div display={ hideFromDelete }>
 			<h2> { category } </h2>
-			<h3>{ title } by {author }</h3>
+			<h3>{ title } by {author }, posted @ { formattedDated }</h3>
 			<p> { body } </p>  
 			<div> { voteScore } </div>
 		</div>
