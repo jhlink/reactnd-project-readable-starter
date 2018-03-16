f7ae4d35fa735aca353c605e9d72856119cba4f4
James Kim <jhlink@users.noreply.github.com>
2018-03-15 23:58:08 -0400

    style: Extend babel-eslint to address class field/property initializer eslint-error

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es6: true
   },
-  extends: ["plugin:react/recommended"],
+  extends: ["babel-eslint", "plugin:react/recommended"],
   parserOptions: {
     ecmaFeatures: {
       experimentalObjectRestSpread: true,
