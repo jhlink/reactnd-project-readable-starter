c09cd5517bf93d8767d3fd76eb928a41f1722e53
James Kim <jhlink@users.noreply.github.com>
2018-03-18 21:17:30 -0400

    feat: Create GetAllCategories action, action creator, and thunk

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,8 +1,25 @@
+import * as ServerAPI from '../utils/serverAPI.js';
+
 export const LOAD_CATEGORY_POSTS = 'LOAD_CATEGORY_POSTS';
+export const GET_ALL_CATEGORIES = 'GET_ALL_CATEGORIES';
 
 export const loadCategoryPosts = ( categoryId ) => {
 	return {
 		type: LOAD_CATEGORY_POSTS,
 		categoryId
 	};
+};
+
+export const getCategories = ( categories ) => {
+  return {
+    type: GET_ALL_CATEGORIES,
+    categories
+  };
+};
+
+// Thunks 
+
+export const FetchCategories = () => dispatch => {
+  return ServerAPI.GetCategories()
+    .then(categories => dispatch(getCategories(categories)))
 };
