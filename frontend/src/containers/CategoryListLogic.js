1116519a96523a6899f01bad8c42554e76a17df7
James Kim <jhlink@users.noreply.github.com>
2018-03-18 21:57:07 -0400

    refactor: Remove posts and connect from CategoryListLogic

--- frontend/src/containers/CategoryListLogic.js
+++ frontend/src/containers/CategoryListLogic.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import CategoryList from '../components/CategoryList';
-import { connect } from 'react-redux';
-import { loadCategoryPosts } from '../actions';
 
 class CategoryListLogic extends Component {
-
-  handleSelectedCategory = (categoryName) => {
-    console.log(categoryName);
-    this.props.dispatch(loadCategoryPosts(categoryName))
-  }
 
 	render() {
 		const { categories } = this.props;
 
-		return <CategoryList categories={ categories } 
-      handleSelectedCategory={ this.handleSelectedCategory }/>;
+		return <CategoryList categories={ categories }/> 
       
 	}
 };
@@ -24,4 +16,4 @@ CategoryListLogic.propTypes = {
 	categories: PropTypes.array.isRequired
 };
 
-export default connect()(CategoryListLogic);
+export default CategoryListLogic;
