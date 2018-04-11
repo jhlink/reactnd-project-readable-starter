8f9037c462d3b8e826c5a3370742a60da2fe3485
James Kim <jhlink@users.noreply.github.com>
2018-04-11 04:51:27 -0400

    style: Embellish comment components further

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -5,11 +5,14 @@ const Comment = (props) => {
 	console.log(props.comment);
   
 	const { id, parentId, timestamp, body, author, voteScore, deleted, parentDeleted } = props.comment;
+  const hideFromDelete = parentDeleted || deleted;
+  const formattedDated = new Date(timestamp).toLocaleString();
 
 	return (
-		<div className="comment content">
-			<p> { body } -- { author } </p>  
-			<div> { voteScore } </div>
+		<div display={ hideFromDelete } className="comment content">
+			<p className="commentBody"> { body } </p>  
+      <h3 className="commentAuthor"> by {author } - { formattedDated } </h3>
+			<div className="commentVoteScore"> Vote Score:  { voteScore } </div>
 		</div>
 	);
 };
