d8d8abdcb81cd3746b609990dcc76b92996b17bf
James Kim <jhlink@users.noreply.github.com>
2018-04-11 01:56:59 -0400

    feat: Add PostDetailViewLogic and NavLinks to Posts in PostList

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Post from './Post';
+import { Route, Switch, NavLink } from 'react-router-dom';
+import PostDetailViewLogic from '../containers/PostDetailViewLogic';
 
 const PostList = ( props ) =>  {
 	const { posts } = props; 
@@ -10,10 +11,17 @@ const PostList = ( props ) =>  {
 		  <ul>
 		  	{posts.map((post) => (
 		  		<li key={post.id} className="post">
-		  			<Post post={ post }/>
+              <NavLink 
+                to={'/' +  post.id + '/comments'}
+                className="nav link"
+              > { post.title } 
+              </NavLink>
 		  		</li>
 		  	))}
 		  </ul>
+      <Switch> 
+        <Route path={`/:postId?/comments`} component={ PostDetailViewLogic }/>
+      </Switch>
     </div>
 	);
 };
