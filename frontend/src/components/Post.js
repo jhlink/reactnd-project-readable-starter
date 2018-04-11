ca0218ec8e9d0f1e876ff5031d656632c4c5e9d4
James Kim <jhlink@users.noreply.github.com>
2018-04-11 04:02:26 -0400

    feat: Integrate CommentList into Post component

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import CommentList from './CommentList';
 
 const Post = (props) => {
 	const { timestamp, title, body, author, category, voteScore, deleted } = props.post;
+  const { comments } = props;
 	const hideFromDelete = deleted ? 'none' : 'block';
 	const formattedDated = new Date(timestamp).toLocaleString();
+
+  console.log(props)
 
 	return (
 		<div display={ hideFromDelete }>
 			<h2 className="postHeader"> { title } </h2>
       <h3 className="postSubheader"> by {author } - { formattedDated } </h3>
-			<p className="postBody"> { body } </p>  
+			<p className="postBody"> { body } + { category } </p>  
 			<div className="postVoteScore"> Vote Score:  { voteScore } </div>
+      <CommentList comments={ comments }/>
 		</div>
 	);
 };
