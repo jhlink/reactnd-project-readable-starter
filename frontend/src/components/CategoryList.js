3c615548f95d36d7114591681bf6fca4ccff289d
James Kim <jhlink@users.noreply.github.com>
2018-04-16 06:12:52 -0400

    docs: Add question regarding separation of Route and Navlink to AddPost component

--- frontend/src/components/CategoryList.js
+++ frontend/src/components/CategoryList.js
@@ -23,6 +23,16 @@ const CategoryList = (props) => {
       </div>
 
       <Switch> 
+        {/*
+         / TODO: From a production standpoint, this feels wrong. I feel that the AddPost compoenent
+         /  would make better logical sense to put in PostListLogic, which is where the NavLink to
+         /  AddPost exists. This separation of Route and NavLink components just feels like a huge
+         /  red flag, yet at the same time this method achieves the purpose of "hiding" the list of
+         /  posts when adding a new post in a very clean and reusable fashion. 
+         /  This may be subjective, but in general if you're navigating to a new page or appending a new
+         /  URL path, is it okay to place the NavLink in a separate component and add the Route
+         /  to that component to the parent of that component?
+        */}
         <Route path={`/:categoryId?/addpost`} component={ AddPost }/>
         <Route exact path={`/:categoryId?`} component={ PostListLogic }/>
       </Switch>
