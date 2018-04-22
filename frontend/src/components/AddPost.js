97e6999ccc2aea729099653aabfe394ab373225b
James Kim <jhlink@users.noreply.github.com>
2018-04-21 20:16:43 -0400

    feat: Utilize form submit handler and type check handlePostSubmit

--- frontend/src/components/AddPost.js
+++ frontend/src/components/AddPost.js
@@ -2,14 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const AddPost = (props) => {
-	//const { timestamp, title, body, author, category, voteScore, deleted } = props.post;
-  //const { comments } = props;
-	//const hideFromDelete = deleted ? 'none' : 'block';
-	//const formattedDated = new Date(timestamp).toLocaleString();
   
 	return (
 		<div className="column content">
-        <form className="addPostForm">
+        <form className="addPostForm" onSubmit={ props.handlePostSubmit }>
             <input type="text" name="title" placeholder="Title"/>
             <input type="text" name="author" placeholder="Who are you?"/>
             <textarea type="text" name="body" placeholder="Enter text here..."/>
@@ -20,7 +16,7 @@ const AddPost = (props) => {
 };
 
 AddPost.propTypes = {
-	//post: PropTypes.object.isRequired
+	handlePostSubmit: PropTypes.func.isRequired
 };
 
 export default AddPost;
