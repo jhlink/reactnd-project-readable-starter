4bdef40b679a2293f32721c3e88d32be81a0784b
James Kim <jhlink@users.noreply.github.com>
2018-04-22 14:07:03 -0400

    feat: Add title, body, and author props to PostForm if there is pre-existing data

--- frontend/src/components/PostForm.js
+++ frontend/src/components/PostForm.js
@@ -2,14 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const PostForm = (props) => {
-  
+  const { categoryId, handlePostSubmit, title, author, body } = props;  
+
 	return (
 		<div className="column content">
-        <h2>New { props.categoryId } Post</h2>
-        <form className="addPostForm" onSubmit={ props.handlePostSubmit }>
-            <input type="text" name="title" placeholder="Title"/>
-            <input type="text" name="author" placeholder="Who are you?"/>
-            <textarea type="text" name="body" placeholder="Enter text here..."/>
+        <h2>New { categoryId } Post</h2>
+        <form className="addPostForm" onSubmit={ handlePostSubmit }>
+            <input type="text" name="title" placeholder="Title" value={ title } />
+            <input type="text" name="author" placeholder="Who are you?" value={ author } />
+            <textarea type="text" name="body" placeholder="Enter text here..." value={ body } />
             <button>Add Post</button>
         </form>
 		</div>
