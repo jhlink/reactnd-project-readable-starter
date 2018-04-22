40677866cfe2f7b274f93e7429fd313e59475adf
James Kim <jhlink@users.noreply.github.com>
2018-04-22 18:26:29 -0400

    feat: Replace defaultValue with value/onChange implementation and condense props

--- frontend/src/components/PostForm.js
+++ frontend/src/components/PostForm.js
@@ -2,15 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const PostForm = (props) => {
-  const { categoryId, handlePostSubmit, title, author, body } = props;  
+  const { post,
+    handlePostSubmit, 
+    handlePostChange 
+    } = props;
+
+  const {
+    category,
+    title, 
+    author, 
+    body 
+    } = post;  
 
 	return (
 		<div className="column content">
-        <h2>New { categoryId } Post</h2>
+        <h2>New { category } Post</h2>
         <form className="addPostForm" onSubmit={ handlePostSubmit }>
-            <input type="text" name="title" placeholder="Title" defaultValue={ title } />
-            <input type="text" name="author" placeholder="Who are you?" defaultValue={ author } />
-            <textarea type="text" name="body" placeholder="Enter text here..." defaultValue={ body } />
+            <input type="text" name="title" placeholder="Title" value={ title } onChange={e => (
+              handlePostChange(e))} />
+            <input type="text" name="author" placeholder="Who are you?" value={ author } onChange={e => (
+              handlePostChange(e))}  />
+            <textarea type="text" name="body" placeholder="Enter text here..." value={ body } onChange={e => (
+              handlePostChange(e))} />
             <button>Add Post</button>
         </form>
 		</div>
