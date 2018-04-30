64fa711763e42f66743cd864b8cf87a7f005f8c4
James Kim <jhlink@users.noreply.github.com>
2018-04-30 01:20:21 -0400

    fix: Hide sort box with Add Post link

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -26,27 +26,27 @@ const PostList = ( props ) =>  {
 
 	return (
     <div className="column content">
-      <div className="horizJust">
-          <div className="sortblock"> 
-            <p className="sort-header">Sort By</p>
-            <select value={ JSON.stringify(sortValues.formSortCriteria) } 
-                    onChange={sortValues.sortHandler.bind(this)}
-                    className="sort">
-              <option value={ jsonify('voteScore', 'desc') }>Most Popular</option>
-              <option value={ jsonify('voteScore', 'asc') }>Least Popular</option>
-              <option value={ jsonify('timestamp', 'desc') }>Newest Post</option>
-              <option value={ jsonify('timestamp', 'asc') }>Oldest Post</option>
-            </select>
-          </div>
         { showAddPost && ( 
-          <NavLink 
-            to={match.url + '/addpost'}
-            className="nav link addpost"
-           >
-          Add Post
-          </NavLink>
+          <div className="horizJust">
+            <div className="sortblock"> 
+              <p className="sort-header">Sort By</p>
+              <select value={ JSON.stringify(sortValues.formSortCriteria) } 
+                      onChange={sortValues.sortHandler.bind(this)}
+                      className="sort">
+                <option value={ jsonify('voteScore', 'desc') }>Most Popular</option>
+                <option value={ jsonify('voteScore', 'asc') }>Least Popular</option>
+                <option value={ jsonify('timestamp', 'desc') }>Newest Post</option>
+                <option value={ jsonify('timestamp', 'asc') }>Oldest Post</option>
+              </select>
+            </div>
+            <NavLink 
+              to={match.url + '/addpost'}
+              className="nav link addpost"
+             >
+            Add Post
+            </NavLink>
+          </div>
         )}
-        </div>
 		  <ul>
 		  	{postsToShow.map((post) => (
 		  		<li key={post.id} className="post">
