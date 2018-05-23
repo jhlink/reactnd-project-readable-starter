5920b264df063612a74ab51bdc96d078dc345291
James Kim <jhlink@users.noreply.github.com>
2018-05-23 01:18:01 -0400

    style: Fix ridiculous indent error

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,22 +1,28 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true
-  },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  parserOptions: {
-    ecmaVersion: 6,
-    ecmaFeatures: {
-      jsx: true
-    },
-    sourceType: "module"
-  },
-  plugins: ["react"],
-  rules: {
-    indent: ["error", "tab"],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "always"],
-    "no-console": "off" 
-  }
+	env: {
+		browser: true,
+		es6: true
+	},
+	extends: ['eslint:recommended', 'plugin:react/recommended'],
+	parser: 'babel-eslint',
+	parserOptions: {
+		ecmaVersion: 7,
+		ecmaFeatures: {
+			jsx: true
+		},
+		sourceType: 'module'
+	},
+	plugins: ['react'],
+	rules: {
+		indent: ['error', 2],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+		'no-console': 'off' 
+	},
+	'settings': {
+		'react': {
+			'version': '15.0'
+		}
+	}
 };
