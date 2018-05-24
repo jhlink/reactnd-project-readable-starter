7efc119ed20261609b6762fa7dbac7648de01e96
James Kim <jhlink@users.noreply.github.com>
2018-05-24 19:41:26 -0400

    style: Adjust spacing for Post component file

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -5,48 +5,48 @@ import { Route, Switch, NavLink } from 'react-router-dom';
 import PostFormLogic from '../containers/PostFormLogic';
 
 const Post = (props) => {
-	const { timestamp, title, body, author, category, voteScore, deleted } = props.post;
-	const { comments, match, location } = props;
-	const hideFromDelete = deleted ? 'none' : 'block';
-	const formattedDated = new Date(timestamp).toLocaleString();
-	const isEditPost = location.pathname !== match.url + '/editpost';
+  const { timestamp, title, body, author, category, voteScore, deleted } = props.post;
+  const { comments, match, location } = props;
+  const hideFromDelete = deleted ? 'none' : 'block';
+  const formattedDated = new Date(timestamp).toLocaleString();
+  const isEditPost = location.pathname !== match.url + '/editpost';
 
-	return (
-		<div>
-			{ isEditPost && (
-				<div display={ hideFromDelete } className="column content">
-					<h2 className="postHeader"> { title } </h2> 
-					<div className="horizJust">
-						<h3 className="postSubheader"> by {author } - { formattedDated } </h3>
-						<NavLink 
-							to={match.url + '/editpost'}
-							className="nav link edit"
-						> Edit  
-						</NavLink>
-					</div>
-					<p className="postBody"> { body } + { category } </p>  
-					<div className="postVoteScore"> Vote Score:  { voteScore } </div>
-					<div className="commentSectoin">
-						<NavLink 
-							to={match.url + '/addcomment'}
-							className="nav link edit"
-						> Edit  
-						</NavLink>
-						<CommentList comments={ comments }/>
-					</div>
-				</div>
-			)}
-			<Switch>
-				<Route path={match.url + '/editpost'} render={props => (
-					<PostFormLogic post={props.post} {...props}/>  
-				)}/>
-			</Switch>
-		</div>
-	);
+  return (
+    <div>
+      { isEditPost && (
+        <div display={ hideFromDelete } className="column content">
+          <h2 className="postHeader"> { title } </h2> 
+          <div className="horizJust">
+            <h3 className="postSubheader"> by {author } - { formattedDated } </h3>
+            <NavLink 
+              to={match.url + '/editpost'}
+              className="nav link edit"
+            > Edit  
+            </NavLink>
+          </div>
+          <p className="postBody"> { body } + { category } </p>  
+          <div className="postVoteScore"> Vote Score:  { voteScore } </div>
+          <div className="commentSectoin">
+            <NavLink 
+              to={match.url + '/addcomment'}
+              className="nav link edit"
+            > Edit  
+            </NavLink>
+            <CommentList comments={ comments }/>
+          </div>
+        </div>
+      )}
+      <Switch>
+        <Route path={match.url + '/editpost'} render={props => (
+          <PostFormLogic post={props.post} {...props}/>  
+        )}/>
+      </Switch>
+    </div>
+  );
 };
 
 Post.propTypes = {
-	post: PropTypes.object.isRequired
+  post: PropTypes.object.isRequired
 };
 
 export default Post;
