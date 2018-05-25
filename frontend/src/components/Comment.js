3560d019f700da7ac461b9ec4b83af6bb6df714a
James Kim <jhlink@users.noreply.github.com>
2018-05-25 03:12:52 -0400

    style: Adjust spacing for Comment component file

--- frontend/src/components/Comment.js
+++ frontend/src/components/Comment.js
@@ -2,23 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Comment = (props) => { 
-	console.log(props.comment);
-  
-	const { id, parentId, timestamp, body, author, voteScore, deleted, parentDeleted } = props.comment;
+  const { id, parentId, timestamp, body, author, voteScore, deleted, parentDeleted } = props.comment;
   const hideFromDelete = parentDeleted || deleted;
   const formattedDated = new Date(timestamp).toLocaleString();
 
-	return (
-		<div display={ hideFromDelete } className="comment content">
-			<p className="commentBody"> { body } </p>  
+  return (
+    <div display={ hideFromDelete } className="comment content">
+      <p className="commentBody"> { body } </p>  
       <h3 className="commentAuthor"> by {author } - { formattedDated } </h3>
-			<div className="commentVoteScore"> Vote Score:  { voteScore } </div>
-		</div>
-	);
+      <div className="commentVoteScore"> Vote Score:  { voteScore } </div>
+    </div>
+  );
 };
 
 Comment.propTypes = {
-	comment: PropTypes.object.isRequired
+  comment: PropTypes.object.isRequired
 };
 
 
