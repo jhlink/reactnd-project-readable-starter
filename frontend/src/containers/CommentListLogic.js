34571103fa583b2660ffc670563b26f5bdadb934
James Kim <jhlink@users.noreply.github.com>
2018-05-27 23:22:12 -0400

    fix: Remove sort implementation from CommentListLogic

--- frontend/src/containers/CommentListLogic.js
+++ frontend/src/containers/CommentListLogic.js
@@ -3,21 +3,15 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import CommentList from '../components/CommentList';
 import { FetchPostComments, PostVote } from '../actions';
-import _ from 'lodash';
 
 class CommentListLogic extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      comments: [],
-      formSortCriteria: { 
-        criteria: 'voteScore',
-        order: 'desc'
-      } 
+      comments: []
     };
     this.handleVote = this.handleVote.bind(this);
-    this.handleSelectedSort = this.handleSelectedSort.bind(this);
   }
 
  handleCommentsDispatch = ( postId ) => {
@@ -30,19 +24,6 @@ class CommentListLogic extends Component {
     e.preventDefault();
     const voteType = { option: e.target.id };
     this.props.dispatch(PostVote(commentId, voteType));
-  }
-
-  handleSelectedSort = ( e ) => {
-    e.preventDefault();
-    const sortCrit = JSON.parse(e.target.value);
-    this.setState({ formSortCriteria: sortCrit });
-  }
-
-  commentSorter = (comments) => {
-    const ncomments = _.orderBy(comments, 
-      this.state.formSortCriteria.criteria, 
-      this.state.formSortCriteria.order);
-    return ncomments;
   }
 
   componentWillMount() {
@@ -58,16 +39,12 @@ class CommentListLogic extends Component {
   
   render() {
     const { comments } = this.state;
-    const sorted = this.commentSorter(comments);
-    const sortValues = { 
-      formSortCriteria: this.state.formSortCriteria, 
-      sortHandler: this.handleSelectedSort 
-    };
+    const { match } = this.props;
 
     return <CommentList 
       comments={ comments } 
-      voteHandler={ this.handleVote }
-      sortValues={ sortValues }/>;
+      match={ match }
+      voteHandler={ this.handleVote }/>;
   }
 }
 
