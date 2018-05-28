59c1d5be12e3dd6f282350801e3038f145144ce3
James Kim <jhlink@users.noreply.github.com>
2018-05-28 02:22:57 -0400

    feat: Create EditDeleteBlock due to DRY rule

---
+++ frontend/src/components/EditDeleteBlock.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+
+const EditDeleteBlock = (props) => {
+  const { editLinkPath, deleteHandler } = props;
+  
+  return  (
+    <div className="structure-flex-row"> 
+      <NavLink 
+        to={editLinkPath}
+        className="nav-link edit">
+       Edit 
+      </NavLink>
+      <button 
+        onClick={deleteHandler.bind(this)}
+        className="nav-link edit">
+        Delete
+      </button>
+    </div>
+  );
+};
+
+EditDeleteBlock.propTypes = {
+};
+
+export default EditDeleteBlock;
