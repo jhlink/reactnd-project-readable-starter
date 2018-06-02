132b9f4ad7aaff6e0436afd3c12336fd497d9415
James Kim <jhlink@users.noreply.github.com>
2018-06-02 15:24:10 -0400

    feat: Create Switch/Route to handle Redirects to /404 and nominal web app operations

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import CategoryList from './CategoryList';
+import NotFound from './NotFound';
 import PropTypes from 'prop-types';
+import { Switch, Route } from 'react-router-dom';
 
 const App = (props) => { 
   const { categories } = props;
 
   return (
     <div>
-      <h1 className="title-header">Categories</h1>
-      <CategoryList categories={ categories } />
+      <Switch>
+        <Route exact path='/404' component={ NotFound } />
+        <Route path="/" render={ () => (
+          <div>
+            <h1 className="title-header">Categories</h1>
+            <CategoryList categories={ categories } />
+          </div>
+        )}/>
+      </Switch>
     </div>
   ); 
 }; 
