3db127b5406f0e32289a17af05aea2eae1bde607
James Kim <jhlink@users.noreply.github.com>
2018-06-02 22:14:06 -0400

    fix: Hide sorter and AddPost in PostDetail view

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -11,6 +11,7 @@ const PostList = ( props ) =>  {
   const postsToShow = location.pathname === match.url ? posts : [];
   const postsNotDeleted = postsToShow.filter(post => !post.deleted); 
   const isEmpty = postsToShow.length === 0 ? {display: 'none'} : {};
+  const postListInView = location.pathname === match.url;
 
   const categoryUrl = (postCategory) => {
     return match.url !== '/' ? match.url : postCategory;
@@ -25,24 +26,26 @@ const PostList = ( props ) =>  {
 
   return (
     <div className="main-body">
-      <div className="structure-flex-row">
-        <div className="structure-flex-col"> 
-          <h2 className="sort-header">Sort By</h2>
-          <select value={ JSON.stringify(sortValues.formSortCriteria) } 
-            onChange={sortValues.sortHandler.bind(this)}
-            className="sort">
-            <option value={ jsonify('voteScore', 'desc') }>Most Popular</option>
-            <option value={ jsonify('voteScore', 'asc') }>Least Popular</option>
-            <option value={ jsonify('timestamp', 'desc') }>Newest Post</option>
-            <option value={ jsonify('timestamp', 'asc') }>Oldest Post</option>
-          </select>
-        </div>
-        <NavLink 
-          to={categoryUrl('') + '/addpost'}
-          className="nav-link post">
+      { postListInView && (
+        <div className="structure-flex-row">
+          <div className="structure-flex-col"> 
+            <h2 className="sort-header">Sort By</h2>
+            <select value={ JSON.stringify(sortValues.formSortCriteria) } 
+              onChange={sortValues.sortHandler.bind(this)}
+              className="sort">
+              <option value={ jsonify('voteScore', 'desc') }>Most Popular</option>
+              <option value={ jsonify('voteScore', 'asc') }>Least Popular</option>
+              <option value={ jsonify('timestamp', 'desc') }>Newest Post</option>
+              <option value={ jsonify('timestamp', 'asc') }>Oldest Post</option>
+            </select>
+          </div>
+          <NavLink 
+            to={categoryUrl('') + '/addpost'}
+            className="nav-link post">
             Add Post
-        </NavLink>
-      </div>
+          </NavLink>
+        </div>
+      )}
       <ul style={ isEmpty }>
         {postsNotDeleted.map((post) => (
           <li key={post.id} className="post">
